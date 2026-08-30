2d89e0b21224148ecdd05eae67cd1649cfc07d75
jeremias121210-stack <jeremias121210@gmail.com>
2026-08-30 17:09:30 -0300

    Fix typo in JSON key and update HTML file copy

--- build-tiles.sh
+++ build-tiles.sh
@@ -23,7 +23,7 @@ cat > public/tiles/barros-map-pe.version.json << FIMJSON
   "region": "Nordeste (recorte Pernambuco)",
   "buildDate": "$(date -u +%Y-%m-%dT%H:%M:%SZ)",
   "osmExtractDate": null,
-  "osmSourceRetrievedAt": "${OSM_RETRIEVED_AT}",
+  "iosmSourceRetrievedAt": "${OSM_RETRIEVED_AT}",
   "planetilerVersion": "${PLANETILER_VERSION}",
   "planetilerSha256": "${JAR_SHA}",
   "buildSeconds": $((FIM-INICIO)),
@@ -31,5 +31,5 @@ cat > public/tiles/barros-map-pe.version.json << FIMJSON
   "pmtilesSha256": "${SHA}"
 }
 FIMJSON
-cp mapa-corrida-1-7-0.html public/index.html
+cp mapa-corrida-1-7-2.html public/index.html
 echo ">> Mapa gerado: $((TAM/1048576)) MB"
